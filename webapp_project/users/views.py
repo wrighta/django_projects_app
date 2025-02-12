e071baba3a190edfd48bb441f0cb45bc5c4c7e3c
wrighta999 <anne_wright27@hotmail.com>
2025-02-12 14:04:58 +0000

    Still Auth

--- webapp_project/users/views.py
+++ webapp_project/users/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout, authenticate 
 from django.contrib.auth.forms import UserCreationForm 
 from .models import CustomUser
+from .forms import CustomUserCreationForm
 from django.contrib import messages
 
 def user_list(request):
@@ -9,32 +10,33 @@ def user_list(request):
     return render(request, 'users/user_list.html', {'users': users})
 
 
-def register(request): 
-    if request.method == "POST": 
-        form = UserCreationForm(request.POST) 
-        if form.is_valid(): 
-            user = form.save() 
-            login(request, user) 
-            return redirect("project_list")
-    else: 
-        form = UserCreationForm() 
-    return render(request, "users/register.html", {"form": form}) 
+def register(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}! You can now log in.')
+            return redirect('users/login')  # Redirect to the login page after registration
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'users/register.html', {'form': form})
 
 def user_login(request):
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
         user = authenticate(request, username=username, password=password)
-        
         if user is not None:
             login(request, user)
             messages.success(request, f'Welcome back, {username}!')  # Optional: Add a success message
-            return redirect('projects:home')  # Redirect to the home page after login
+            return redirect('projects')  # Redirect to the home page after login
         else:
             messages.error(request, 'Invalid username or password. Please try again.')  # Add an error message
-            return redirect('users:login')  # Redirect back to the login page
+            return redirect('users/login')  # Redirect back to the login page
     else:
-        return render(request, 'users/login.html')
+        return render(request, 'login.html', {'form': form})
+        #return render(request, 'users/login.html')
 
 def user_logout(request): 
     logout(request) 
